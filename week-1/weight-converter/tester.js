e9208da3ac1230a31ee0e1e5dfabab8424e844cc
Noah <nhamblen21@gmail.com>
2025-03-19 15:38:42 -0500

    Format files to begin week-1 assignment

--- week-1/weight-converter/tester.js
+++ week-1/weight-converter/tester.js
@@ -19,9 +19,11 @@ const { spawn } = require("child_process");
 const path = require("path");
 
 const testWeight = "10";
-const expectedOutput = "4.54\n";  // Note: the output will have a trailing newline
+const expectedOutput = "4.54\n"; // Note: the output will have a trailing newline
 
-const testProcess = spawn("node", ["weight-converter.js", testWeight], { cwd: path.dirname(__filename) });
+const testProcess = spawn("node", ["weight-converter.js", testWeight], {
+  cwd: path.dirname(__filename),
+});
 
 testProcess.stdout.on("data", (data) => {
   const output = data.toString();
--- week-1/weight-converter/weight-converter.js
+++ week-1/weight-converter/weight-converter.js
@@ -1,9 +1,9 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
-*/
+ * Author: Noah Hamblen
+ * Date: 3/19/25
+ * File Name: weight-converter.js
+ * Description: script that converts pounds to kilograms
+ */
 
 "use strict";
 
