593b56179791b0521cd147867adf88406e387983
Noah <nhamblen21@gmail.com>
2025-04-09 13:24:31 -0500

    Add script and class

--- week-4/taco-stand-app/src/taco-stand.js
+++ week-4/taco-stand-app/src/taco-stand.js
@@ -9,4 +9,18 @@
 
 const EventEmitter = require("events");
 
-// TODO: Create a TacoStandEmitter class that extends EventEmitter and implements the following methods: serveCustomer, prepareTaco, and handleRush
+class TacoStandEmitter extends EventEmitter {
+  serveCustomer(customer) {
+    this.emit("serve", customer);
+  }
+
+  prepareTaco(taco) {
+    this.emit("prepare", customer);
+  }
+
+  handleRush(rush) {
+    this.emit("rush", rush);
+  }
+}
+
+module.exports = TacoStandEmitter;
