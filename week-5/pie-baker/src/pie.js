2a26e9f671dd92dbf46dd13dc27e512cb8bb8798
Noah <nhamblen21@gmail.com>
2025-04-16 11:45:40 -0500

    Format files to begin week 5 assignment

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,8 +1,8 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Noah Hamblen
+ * Date: 4/16/25
+ * File Name: pie.js
+ * Description: Contains the bakePie function, which checks if essential ingredients (flour, sugar, butter) are present for baking a pie.
  */
 "use strict";
 
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,8 +1,8 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Noah Hamblen
+ * Date: 4/16/25
+ * File Name: pie.spec.js
+ * Description: Includes unit tests for the bakePie function using Jest.
  */
 
 "use strict";
