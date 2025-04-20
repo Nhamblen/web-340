76653efb1b525a7f7ad982c3715ffb4738c346b9
Noah <nhamblen21@gmail.com>
2025-04-20 13:16:07 -0500

    Build jest tests

--- week-6/fantasy-game-character-creation/test/server.spec.js
+++ week-6/fantasy-game-character-creation/test/server.spec.js
@@ -8,3 +8,47 @@ const http = require("http");
 const server = require("../src/server");
 
 // TODO: Implement your tests here
+describe("Fantasy Game Character Creation API", () => {
+  test("create a character with POST /create", (done) => {
+    const data = JSON.stringify({});
+    const req = http.request(
+      {
+        hostname: "localhost",
+        port: 3000,
+        path: "/create?class=Warrior&gender=Male&funFact=One+arm",
+        method: "POST",
+      },
+      (res) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      }
+    );
+
+    req.write(data);
+    req.end();
+  });
+
+  test("confirm character creation with POST /confirm", (done) => {
+    const req = http.request(
+      {
+        hostname: "localhost",
+        port: 3000,
+        path: "/confirm",
+        method: "POST",
+      },
+      (res) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      }
+    );
+
+    req.end();
+  });
+
+  test("retrieve character with GET /view", (done) => {
+    http.get("http://localhost:3000/view", (res) => {
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+  });
+});
