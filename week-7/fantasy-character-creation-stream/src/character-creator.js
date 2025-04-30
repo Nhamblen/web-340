ccc8c0be47b40e276de00d3458dc8d3e47071d28
Noah <nhamblen21@gmail.com>
2025-04-29 21:39:39 -0500

    Add character creator class and read/write

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -1,17 +1,46 @@
-const { Duplex } = require('stream');
+const { Duplex } = require("stream");
 
 class CharacterCreator extends Duplex {
   constructor(options) {
-    super(options);
-    // TODO: Initialize your class here
+    super(options); // Call parent constructor
+    this.data = ""; // Store incoming data for processing
   }
 
+  // Handles data written to the stream
   _write(chunk, encoding, callback) {
-    // TODO: Implement your _write method here
+    const input = chunk.toString().trim(); // Convert buffer to string and trim whitespace
+
+    // Emit an error if input is empty
+    if (!input) {
+      this.emit("error", new Error("Input can't be empty"));
+      callback();
+      return;
+    }
+
+    // Store the input for later transformation in _read
+    this.data += input;
+    callback(); // Signal that the write is complete
   }
 
+  // Called when data is being read from the stream
   _read(size) {
-    // TODO: Implement your _read method here
+    // If we have data, process and push it
+    if (this.data) {
+      // Expected input format: "Class,Gender,FunFact"
+      const [charClass, gender, funFact] = this.data.split(",");
+
+      // Format the output string
+      const output = `You created a ${gender} ${charClass} who ${funFact}.`;
+
+      // Push the output to the readable side
+      this.push(output);
+
+      // Clear stored data
+      this.data = "";
+    } else {
+      // No more data to read
+      this.push(null);
+    }
   }
 }
 
