c8eadb955aa733cfc48b797115c0c48fa451f509
Noah <nhamblen21@gmail.com>
2025-05-12 21:04:43 -0500

    Write first test and code to pass

--- week-9/fantasy-game-characters/src/game-characters.js
+++ week-9/fantasy-game-characters/src/game-characters.js
@@ -1,13 +1,35 @@
 // game-characters.js
 const { spawn } = require("child_process");
+const path = require("path");
 
 class GameCharacters {
-  constructor() {
-    // TODO: Set the script file path
+  constructor(scriptName) {
+    this.scriptPath = path.join(__dirname, scriptName);
   }
 
   getCharacters(callback) {
-    // TODO: Implement this method
+    const child = spawn("node", [this.scriptPath]);
+
+    let data = "";
+    let errorData = "";
+
+    child.stdout.on("data", (chunk) => (data += chunk));
+    child.stderr.on("data", (chunk) => (errorData += chunk));
+
+    child.on("close", (code) => {
+      if (errorData || code !== 0) {
+        return callback(errorData || `Exited with code ${code}`, null);
+      }
+
+      try {
+        const parsedData = JSON.parse(data);
+        callback(null, parsedData);
+      } catch (err) {
+        callback("Failed to parse JSON", null);
+      }
+    });
+
+    child.on("error", (err) => callback(err.message, null));
   }
 }
 
--- week-9/fantasy-game-characters/test/game-characters.spec.js
+++ week-9/fantasy-game-characters/test/game-characters.spec.js
@@ -5,11 +5,16 @@ describe("GameCharacters", () => {
   let gameCharacters;
 
   beforeEach(() => {
-    gameCharacters = new GameCharacters();
+    gameCharacters = new GameCharacters("../src/game-characters-data.js");
   });
 
   test("should return game characters data", (done) => {
-    // TODO: Implement this test
+    gameCharacters.getCharacters((err, data) => {
+      expect(err).toBeNull();
+      expect(data).toEqual(expect.any(Array));
+      expect(data[0]).toHaveProperty("class");
+      done();
+    });
   });
 
   test("should handle an error when the game characters data script is not found", (done) => {
