20f021e1c2a686eccc34a06ec57469beb06481a4
Noah <nhamblen21@gmail.com>
2025-05-12 19:20:54 -0500

    Build array of game characters

--- week-9/fantasy-game-characters/src/game-characters-data.js
+++ week-9/fantasy-game-characters/src/game-characters-data.js
@@ -1,1 +1,8 @@
 // TODO: Implement this script
+console.log(
+  JSON.stringify([
+    { class: "Warrior", gender: "Male", funFact: "Wields a massive axe" },
+    { class: "Mage", gender: "Female", funFact: "Throws snowballs" },
+    { class: "Rogue", gender: "Other", funFact: "Is clumsy" },
+  ])
+);
